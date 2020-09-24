74dbee8865da3d9f95c3e0100be27de29bc640e1
Wilson Guo <z5257866@student.unsw.edu.au>
2020-09-24 17:45:18 +1000

    Generating user Id and Token for login.

--- src/auth.py
+++ src/auth.py
@@ -1,22 +1,28 @@
+
+#global variable to store the user data
 data = {
-	'users': []	
+    
+    'users': [] ,
 
 }
 
+tokenId = 1
+userId = 1
 
 def auth_login(email, password):
 
-	for user in data['users']:
-		if user['email'] == email:
-			if user['password'] == password
-
-				print("Success")
-  
-    return {
-        'u_id': 1,
-        'token': '12345',
-    }
-
+    #Checking for correct input
+    for user in data['users']:
+        if user['email'] == email:
+            if user['password'] == password:
+                ret = {
+                    'u_id': userId,
+                    'token': tokenId,
+                }
+                userId += 1
+                tokenId += 1
+                return ret
+                
 
 def auth_logout(token):
     return {
@@ -25,20 +31,25 @@ def auth_logout(token):
 
 def auth_register(email, password, name_first, name_last):
 
-	newUser = {
+    newUser = {
+        'name_first': name_first,
+        'name_last': name_last,
+        'email': email,
+        'password': password,
+    }
 
-		'email': email,
-		'password': password,
+    #adding the registered user to global dict
+    data['users'].append(newUser)
 
-	}
-
-	data['users'].append(newUser)
+    #adding handle
 
 
     return {
-        'u_id': 1,
-        'token': '12345',
+        'u_id': userId,
+        'token': tokenId,
     }
 
-
-
+# quick tests to see if it works
+def test_login():
+    auth_register('test@email.com', 'password')
+    assert auth_login('test@email.com', 'yes') == True
