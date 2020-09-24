5f2aa561aaaace5f2035745fad08424b084c6a91
Wilson Guo <z5257866@student.unsw.edu.au>
2020-09-24 19:00:24 +1000

    Fixed the unique handle generation.

--- src/auth.py
+++ src/auth.py
@@ -17,6 +17,8 @@ userId = 1
 
 def auth_login(email, password):
 
+    #Doesnt work yet
+
     #Checking for correct input
     for user in data['users']:
         if user['email'] == email:
@@ -50,21 +52,20 @@ def auth_register(email, password, name_first, name_last):
     handle = name_first.lower() + name_last.lower()
     handle = handle[:20]
     
-    #adding the registered user to global dict
-    #along with the user handle
-    data['users'][userId] = {'handle' : handle}
-
     #checking if the handle has been used
     if handle in data['handles'] and len(handle) > 19:
         handle = handle[:-len(str(userId))] + str(userId)
 
-    else:
-        handle = handle + str(userId)
+    elif handle in data['handles']:
+        handle = handle + str(userId - 1)
 
+    #adding the registered user to global dict
+    #adding the handle to the users dictionary?
+    data['users'][userId] = {'handle' : handle}
 
+    #checking if the handle has been used
     data['handles'][handle] = True
 
-    
     ret =  {
         'u_id': userId,
         'token': tokenId,
@@ -76,17 +77,12 @@ def auth_register(email, password, name_first, name_last):
     return ret
 
 # quick tests to see if it works
-# def test_login():
-#     auth_register('test@email.com', 'password')
-#     assert auth_login('test@email.com', 'yes') == True
-
-
 auth_register('test@email.com', 'password', 'Wilson', 'Guo')
 print(data)
 auth_register('test@email.com', 'password', 'Wilson', 'Guo')
 print(data)
-print()
-auth_register('test@email.com', 'password', 'anamethatismorethantwentychars', 'Lasdfas')
-print(data)
-auth_register('test@email.com', 'password', 'anamethatismorethantwentychars', 'Lasdfas')
-print(data)
+# auth_login('test@email.com', 'password')
+# auth_register('test@email.com', 'password', 'anamethatismorethantwentychars', 'Lasdfas')
+# print(data)
+# auth_register('test@email.com', 'password', 'anamethatismorethantwentychars', 'Lasdfas')
+# print(data)
