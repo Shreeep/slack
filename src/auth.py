e318394d63bccb0f0890d5c951a7804fd0d9a13e
Wilson Guo <z5257866@student.unsw.edu.au>
2020-09-24 16:57:28 +1000

    Adding in basic register.

--- src/auth.py
+++ src/auth.py
@@ -1,8 +1,22 @@
+data = {
+	'users': []	
+
+}
+
+
 def auth_login(email, password):
+
+	for user in data['users']:
+		if user['email'] == email:
+			if user['password'] == password
+
+				print("Success")
+  
     return {
         'u_id': 1,
         'token': '12345',
     }
+
 
 def auth_logout(token):
     return {
@@ -10,7 +24,21 @@ def auth_logout(token):
     }
 
 def auth_register(email, password, name_first, name_last):
+
+	newUser = {
+
+		'email': email,
+		'password': password,
+
+	}
+
+	data['users'].append(newUser)
+
+
     return {
         'u_id': 1,
         'token': '12345',
     }
+
+
+
