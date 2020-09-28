fc0d395ab01efa366ed06e0e5eab60ad3c29af7d
Vignaraj <z5205992@unsw.edu.au>
2020-09-28 19:04:36 +1000

    added some stuff

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,12 +1,15 @@
 import channel
 import pytest
+import data
 from error import InputError
 
-def test_echo():
-    assert echo.echo("1") == "1", "1 == 1"
-    assert echo.echo("abc") == "abc", "abc == abc"
-    assert echo.echo("trump") == "trump", "trump == trump"
+def test_channel_invite():
+    channel.channel_invite("faweebawoowaba",123,456)
 
-def test_echo_except():
+def test_channel_details():
+    with pytest.raises(InputError) as e:
+        assert echo.echo("echo")
+
+def test_channel_messages():
     with pytest.raises(InputError) as e:
         assert echo.echo("echo")
