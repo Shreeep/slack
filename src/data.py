fbc9aa2d931e74e91af1e84ae9018caea75bfa2f
Vignaraj <z5205992@unsw.edu.au>
2020-09-28 19:01:51 +1000

    data file

---
+++ src/data.py
@@ -0,0 +1,23 @@
+data = {
+    'users': [
+        {
+            'id': 1,
+            'name' : 'user1',
+        },
+        {
+            'id': 2,
+            'name' : 'user2',
+        },
+    ],
+    'channels': [
+        {
+            'id': 1,
+            'name' : 'channel1',
+        },
+        {
+            'id': 2,
+            'name' : 'channel2',
+        },
+    ],
+    'token': 'exampleToken',
+}
