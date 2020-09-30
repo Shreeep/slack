0ce4019624facec1778a1bc6ad8f0e6133aba8d2
Wilson Guo <z5257866@student.unsw.edu.au>
2020-09-30 23:58:20 +1000

    Changed to use data file

--- src/auth.py
+++ src/auth.py
@@ -1,41 +1,24 @@
 import re
-
-#global variable to store the user data
-data = {
-    
-    'users': {
-
-    },
-
-    'handles' : {
-
-    }
-
-}
-
-tokenId = 1
-userId = 1
+import data
 
 def auth_login(email, password):
 
-    global userId
-    global tokenId
-    emailRegex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+    email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
-    if not re.search(emailRegex,email):
+    if not re.search(email_regex,email):
         raise Exception(f"{email} is not valid")
 
     # Checking for correct input
-    for user in data['users']:
-        if data['users'][user]['email'] == email:
-            if data['users'][user]['password'] == password:
+    for user in data.data['users']:
+        if data.data['users'][user]['email'] == email:
+            if data.data['users'][user]['password'] == password:
                 ret = {
-                    'u_id': userId,
-                    'token': tokenId,
+                    'u_id': data.user_id,
+                    'token': data.token_id,
                 }
 
-                userId += 1
-                tokenId += 1
+                data.user_id += 1
+                data.token_id += 1
                 return ret
             else:
                 raise Exception(f"Password is incorrect")
@@ -48,41 +31,38 @@ def auth_logout(token):
     }
 
 def auth_register(email, password, name_first, name_last):
-   
-    global userId
-    global tokenId
 
     #Generating user handle and limiting to 20 chars
     handle = name_first.lower() + name_last.lower()
     handle = handle[:20]
     
     #checking if the handle has been used
-    if handle in data['handles'] and len(handle) >= 20:
-        handle = handle[:-len(str(userId))] + str(userId)
+    if handle in data.data['handles'] and len(handle) >= 20:
+        handle = handle[:-len(str(data.user_id))] + str(data.user_id)
 
-    elif handle in data['handles']:
-        handle = handle + str(userId)
+    elif handle in data.data['handles']:
+        handle = handle + str(data.user_id)
 
     #saving user info into a new dict
-    newUser = {
+    new_user = {
         'email': email,
         'password': password,
         'handle': handle,
     }
 
     #adding user info to global dict
-    data['users'][userId] = newUser
+    data.data['users'][data.user_id] = new_user
 
     #Saves the handle name
-    data['handles'][handle] = True
+    data.data['handles'][handle] = True
 
     ret =  {
-        'u_id': userId,
-        'token': tokenId,
+        'u_id': data.user_id,
+        'token': data.token_id,
     }
 
-    userId += 1
-    tokenId += 1
+    data.user_id += 1
+    data.token_id += 1
 
     return ret
 
---
+++ src/data.py
@@ -0,0 +1,18 @@
+
+
+
+#global variable to store the user data
+data = {
+    
+    'users': {
+
+    },
+
+    'handles': {
+
+    }
+
+}
+
+token_id = 1
+user_id = 1
