abe1b8b1c9646fca446d4503b9b435f0a169f85c
Shree <z5209699@ad.unsw.edu.au>
2020-09-30 22:10:00 +1000

    channel_join tests added, tests imnproved for channel_leave

--- src/channel_test.py
+++ src/channel_test.py
@@ -6,19 +6,27 @@ import error
 
 #tests for channel_leave
 
-
 def test_channel_leave_success():
     #inputs: token (me), ch_id, u_id
     channel.channel_join('token', 0)
+    result = channels.channels_list(token)
+    assert result = [
+        {
+            'id': 0,
+            'name' : 'channel0',
+        }
+    ]
     #inputs: token, ch_id
     channel.channel_leave('token', 0) # Expect to work since we invited user#0 to channel#0
+    assert result = [
+
+    ]
 
 
 def test_channel_leave_input_error():
     #code for exception where Channel ID is not a valid channel
     with pytest.raises(InputError) as e:
         channel.channel_leave('token', 9999) # Expect fail since channel9999 doesn't exist
-
 
 
 def test_channel_leave_access_error():
@@ -31,18 +39,39 @@ def test_channel_leave_access_error():
 
 
 
-
 #tests for channel_join
 
 def test_channel_join_success():
-    #user joins channel0
-    channel.channel_join('token', 0)
+    #user joins channel1 and channel2
+    channel.channel_join('token', 1)
+    channel.channel_join('token', 2)
+    #assuming channels_list is working properly, result should be a dictionary
+    #of channel0 and channel1, the channels that the user has joined
+    result = channels.channels_list(token)
+    assert result = [
+        {
+            'id': 1,
+            'name' : 'channel1',
+        },
+        {
+            'id': 2,
+            'name' : 'channel2',
+        },
+    ]
+
 
 def test_channel_join_input_error():
-    pass
+    #user attemps to join an invalid channel (doesn't exist)
+    with pytest.raises(InputError) as e:
+        channel.channel_join('token', 9999) # Expect fail since channel9999 was never created
+
 
 def test_channel_join_access_error():
-    pass
+    channel_id = channels.channels_create('token', 'generic_name', False)
+    #user attemps to join a channel which is private && authorised user is not a global owner
+    with pytest.raises(AccessError) as e:
+        #error should be raised if channel with ch_id 1, is a private channel
+        channel.channel_join('token', channel_id) # Expect fail since user0 never joined channel1
 
 
 
