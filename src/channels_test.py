2ef4cb3112fd60b5e1bd67ac646bf1d487d28713
z5206651 <h.hasam@student.unsw.edu.au>
2020-10-01 21:58:24 +1000

    Still working on writing tests - will work on tommorow after tutorial/lab

--- src/channels_test.py
+++ src/channels_test.py
@@ -1,38 +1,47 @@
 import channels
+import channel
 import pytest 
 import error 
 
-#Assumptions for channels_* functions: 
-    #
-data = {
-    'users': [
-        {
-            'id': 1,
-            'name' : 'user1',
-        },
-        {
-            'id': 2,
-            'name' : 'user2',
-        },
-    ],
-    'channels': [
-        {
-            'id': 1,
-            'name' : 'channel1',
-        },
-        {
-            'id': 2,
-            'name' : 'channel2',
-        },
-    ],
-}
+#Notes: 
+    #channels_list is a subset of channels_listall
+    #channels_list and channels_listall return a list of dictionaries, where each dictionary contains {channel_id, name}
+    #Test Cases for channels_list(): 
+        #User is apart of a channel - returns that channel 
+        #No channels the user is apart of - returns an empty list 
+        #User is apart of all channels - returns list of all channels 
+        #User is apart of some channels, not all - returns list which is subset of all channels 
+    #Test Cases for channels_listall():
+        #No channels asscociated with the token - returns empty list
+    #Test Cases for channels_create(): 
+        #Channel name alredy exists 
+        #Channel name is above 20 characters 
 
 def test_channels_list():
-    assert channels.channels_list() == # Data from global variable or data.py??
+    test_channel_one = channels.channels_create('random token', 'Channel #1', True)
+    channel.channel_join('random token', test_channel_one) #does the token tell us which user to join the channel?? 
+    result = channels.channels_list('random token')
+    assert result == {
+        'channels': [
+        	{
+        		'channel_id': test_channel_one,
+        		'name': 'Channel #1',
+        	}
+        ],
+    }
+
+def test_channels_list_empty(): 
+    assert channels.channels_list('token') == {}
 
 def test_channels_listall():
-    assert channels.channels_listall() == ##
-def test_channels_create():
-    # Test will need an exception - will read up on exceptions and write test
+    assert channels.channels_listall() == 
 
+def test_channels_listall_empty():
+    assert == {}
 
+def test_channels_create_public():
+    new_channel = channels.channels_create('token', 'My Channel', True)
+    assert new_channel == ch_id
+def test_channels_create_private():
+
+def test_channels_create_already_exists():
