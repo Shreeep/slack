43a885a8449aa05e4bb240395085a19f843dfd7b
Wilson Guo <z5257866@student.unsw.edu.au>
2020-10-01 23:46:56 +1000

    Added all inputError into each auth function. Fixed Exceptions. Finished logout function. Added token to the data dictionary.

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,6 @@
 import re
 import data
+from error import InputError, AccessError
 
 email_regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
@@ -7,7 +8,7 @@ def auth_login(email, password):
 
     #checking if valid email
     if not re.search(email_regex,email):
-        raise Exception(f"{email} is not valid")
+        raise InputError
 
     # Checking for correct input
     for user in data.data['users']:
@@ -18,26 +19,37 @@ def auth_login(email, password):
                     'token': data.token_id,
                 }
 
+                # Adding to the token dictionary
+                data.data['token'][data.token_id] = data.user_id
+
                 data.user_id += 1
                 data.token_id += 1
                 return ret
             else:
-                raise Exception(f"Password is incorrect")
+                raise InputError
                 
-    raise Exception(f"{email} is not registered")
+    raise InputError
 
 def auth_logout(token):
-    return {
-        'is_success': True,
-    }
+
+    # for token in data.data['token']:
+    if token in data.data['token']:
+        data.data['token'].pop(token)
+        return {
+            'is_success': True,
+        }
+    else:
+        return {
+            'is_success': False,
+        }
 
 def auth_register(email, password, name_first, name_last):
 
-    #checking if valid email
+    # checking if valid email
     if not re.search(email_regex,email):
-        raise Exception(f"{email} is not valid")
+        raise InputError
 
-    #Generating user handle and limiting to 20 chars
+    # Generating user handle and limiting to 20 chars
     handle = name_first.lower() + name_last.lower()
     handle = handle[:20]
     
@@ -48,23 +60,43 @@ def auth_register(email, password, name_first, name_last):
     elif handle in data.data['handles']:
         handle = handle + str(data.user_id)
 
-    #saving user info into a new dict
+    # saving user info into a new dict
     new_user = {
         'email': email,
         'password': password,
         'handle': handle,
     }
 
-    #adding user info to global dict
+    # checking whether the email has been registered before
+    for user in data.data['users']:
+        if new_user['email'] == data.data['users'][user]['email']:
+            raise InputError
+
+    # Checking whether the password is valid
+    if len(new_user['password']) < 7:
+        raise InputError
+
+    # first name is between 1 - 50
+    if len(name_first) < 1 or len(name_first) > 50: 
+        raise InputError
+
+    # last name is between 1 - 50
+    if len(name_last) < 1 or len(name_last) > 50:
+        raise InputError
+
+    # adding user info to global dict
     data.data['users'][data.user_id] = new_user
 
-    #Saves the handle name
+    # Saves the handle name
     data.data['handles'][handle] = True
 
     ret =  {
         'u_id': data.user_id,
         'token': data.token_id,
     }
+
+    # Adding to the token dictionary
+    data.data['token'][data.token_id] = data.user_id
 
     data.user_id += 1
     data.token_id += 1
--- src/auth_test.py
+++ src/auth_test.py
@@ -1,42 +1,43 @@
 import auth
 import pytest
+from error import InputError, AccessError
 
 def test_login_success():
-	register1 = auth.auth_register("test@email.com", "password", "Wilson", "Guo")
-	register2 = auth.auth_register("working@email.com", "workingPassword", "Test", "Name")
-	assert auth.auth_login("test@email.com", "password") == {"u_id": 3, "token": 3}
-	assert auth.auth_login("working@email.com", "workingPassword") == {"u_id": 4, "token": 4}
+    register1 = auth.auth_register("test@email.com", "password", "Wilson", "Guo")
+    register2 = auth.auth_register("working@email.com", "workingPassword", "Test", "Name")
+    assert auth.auth_login("test@email.com", "password") == {"u_id": 3, "token": 3}
+    assert auth.auth_login("working@email.com", "workingPassword") == {"u_id": 4, "token": 4}
 
 
 def test_login_fail():
-	register1 = auth.auth_register("test1@email.com", "password", "Wilson", "Guo")
-	with pytest.raises(Exception) as e:
-		auth.auth_login("wrong@email.com", "password") 
+    register1 = auth.auth_register("test1@email.com", "password", "Wilson", "Guo")
+    with pytest.raises(InputError) as e:
+        assert auth.auth_login("wrong@email.com", "password") 
 
-	with pytest.raises(Exception) as e:
-		auth.auth_login("test1@email.com", "wrongpassword")
+    with pytest.raises(InputError) as e:
+        assert auth.auth_login("test1@email.com", "wrongpassword")
 
-	with pytest.raises(Exception) as e:
-		auth.auth_login("notvalidemail", "password")
+    with pytest.raises(InputError) as e:
+        assert auth.auth_login("notvalidemail", "password")
 
 
 def test_register_success():
-	assert auth.auth_register("alreadyused@email.com", "password", "New", "Person") == {"u_id": 6, "token": 6}
-	assert auth.auth_register("new@email.com", "newpassword", "Another", "Person") == {"u_id": 7, "token": 7}
+    assert auth.auth_register("alreadyused@email.com", "password", "New", "Person") == {"u_id": 6, "token": 6}
+    assert auth.auth_register("new@email.com", "newpassword", "Another", "Person") == {"u_id": 7, "token": 7}
 
 
 def test_register_fail():
-	with pytest.raises(Exception) as e:
-		auth.auth_register("anotherinvalidemail", "password", "Register", "Fails")
-		
-	with pytest.raises(Exception) as e:
-		auth.auth_register("alreadyused@email.com", "password", "Person", "Another")
+    with pytest.raises(InputError) as e:
+        assert auth.auth_register("anotherinvalidemail", "password", "Register", "Fails")
+        
+    with pytest.raises(InputError) as e:
+        auth.auth_register("alreadyused@email.com", "password", "Person", "Another")
 
-	with pytest.raises(Exception) as e:
-		auth.auth_register("shortpassword@email.com", "12345", "Short", "Password")
+    with pytest.raises(InputError) as e:
+      assert auth.auth_register("shortpassword@email.com", "12345", "Short", "Password")
 
-	with pytest.raises(Exception) as e:
-		auth.auth_register("namelong@email.com", "password", "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "surname")
+    with pytest.raises(InputError) as e:
+      assert auth.auth_register("namelong@email.com", "password", "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "surname")
 
-	with pytest.raises(Exception) as e:
-		auth.auth_register("surnamelong@email.com", "password", "name", "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
+    with pytest.raises(InputError) as e:
+     assert  auth.auth_register("surnamelong@email.com", "password", "name", "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz")
