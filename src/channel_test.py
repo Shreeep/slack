513220d6b677aec7ddefe662226e29bf4ea3df45
Shree <z5209699@ad.unsw.edu.au>
2020-10-03 00:41:07 +1000

    channels.py implemented, need to add errors

--- src/channel_test.py
+++ src/channel_test.py
@@ -7,9 +7,10 @@ import error
 #tests for channel_leave
 
 def test_channel_leave_success():
+    #add channel_create
     #inputs: token (me), ch_id, u_id
     channel.channel_join('token', 0)
-    result = channels.channels_list(token)
+    result = channels.channels_list('token')
     assert result = [
         {
             'id': 0,
@@ -42,9 +43,12 @@ def test_channel_leave_access_error():
 #tests for channel_join
 
 def test_channel_join_success():
+    channel_id_1 = channels.channels_create('token', 'new_channel1', True)
+    channel_id_2 = channels.channels_create('token', 'new_channel2', True)
     #user joins channel1 and channel2
-    channel.channel_join('token', 1)
-    channel.channel_join('token', 2)
+
+    channel.channel_join('token', channel_id_1)
+    channel.channel_join('token', channel_id_2)
     #assuming channels_list is working properly, result should be a dictionary
     #of channel0 and channel1, the channels that the user has joined
     result = channels.channels_list(token)
@@ -78,13 +82,58 @@ def test_channel_join_access_error():
 
 #tests for channel_addowner
 def test_channel_addowner_success():
-    #run channel_addowner
-    #use channel_detail to ensure that user is part of the channel
-    pass
+    #create a channel called 'new_channel'
+    ch_id = channels.channels_create('token', 'new_channel', True)
+    #add user7 as owner to this channel
+    channel.channel_addowner('token', ch_id, 7)
+    #auth user joins this channel for channel_details to be useful
+    channel.channel_join('token', ch_id)
+    name, owner_members, all_members = channel.channel_details('token', ch_id)
+
+    #assert that user7 is a part of the owner_members
+    for member in owner_members:
+        new_owner_success = 7 in member.values()
+
+    assert new_owner_success == True
 
 
-def test_channel_addowner_input_error():
-    pass
+
+
+def test_channel_addowner_input_error1():
+    #channel687 has not been created, thus input error
+    with pytest.raises(InputError) as e:
+        channel.channel_addowner('token', 687,6)
+
+
+def test_channel_addowner_input_error2():
+    #create a channel with inputs token, channel_name, isPublic
+    channel_id = channels.channels_create('token', 'new_channel', True)
+    #add user7 as owner to this channel
+    channel.channel_addowner('token', ch_id, 7)
+    #trying to add user7 as an owner to the channel more than once
+    with pytest.raises(InputError) as e:
+        channel.channel_addowner('token', ch_id, 7)
+
 
 def test_channel_addowner_access_error():
+    channel_id = channels.channels_create('token', 'new_channel', True)
+    #raise AccessError if auth user is either:
+    #1. owner of the flockr
+    #2. owner of the channel
+    with pytest.raises(AccessError) as e:
+        channel.channel_join('token', channel_id)
+
+
+
+#tests for channel_removeowner
+def test_channel_removeowner_success():
+    pass
+
+def test_channel_removeowner_InputError1():
+    pass
+
+def test_channel_removeowner_InputError2():
+    pass
+
+def test_channel_removeowner_AccessError():
     pass
--- src/data.py
+++ src/data.py
@@ -1,22 +1,60 @@
 data = {
-    'users': [
-        {
-            'id': 1,
-            'name' : 'user1',
+    'users': {
+        1: {
+            'email': 'asdas@gmail.com',
+            'password': 'password',
+            'name_first': 'name_first',
+            'name_last': 'name_last',
+            'handle_str': 'handle',
+            'is_global_owner' : True
         },
-        {
-            'id': 2,
-            'name' : 'user2',
+        2: {
+            'email': email,
+            'password': password,
+            'name_first': name_first,
+            'name_laste': name_last,
+            'handle_str': handle,
+            'is_global_owner' : False
         },
-    ],
+    },
     'channels': [
         {
             'id': 1,
             'name' : 'channel1',
+            #user1, user2, user3
+            'members' : [
+                {
+                    'u_id': 1,
+                    'name_first': 'Hayden',
+                    'name_last': 'Jacobs',
+                    'is_channel_owner':True
+                },
+                {
+                    'u_id': 2,
+                    'name_first': 'Bob',
+                    'name_last': 'Willy',
+                }
+            ],
+            'owners' : [
+                {
+                    'u_id': 1,
+                    'name_first': 'Hayden',
+                    'name_last': 'Jacobs',
+                }
+            ],
+            'is_public': True,
         },
         {
             'id': 2,
             'name' : 'channel2',
+            #user1, user2, user3
+            'members' : [1, 2, 3],
+            'owners' : [3, 4],
+            'is_public': True,
         },
     ],
+    'tokens': {
+        'token1': 523, #userid
+        'token2': 1234,
+    },
 }
