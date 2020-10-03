a40af3f04da9ed20b717fd56297720fe40801489
Wilson Guo <z5257866@student.unsw.edu.au>
2020-10-03 22:33:00 +1000

    Add extra test cases for auth_register

--- src/auth_test.py
+++ src/auth_test.py
@@ -10,26 +10,50 @@ def test_register_success():
     assert register1['u_id'] != None and register1['token'] != None
     assert register2['u_id'] != None and register2['token'] != None
 
-def test_register_fail():
+def test_register_email_input_fail():
     clear()
     with pytest.raises(InputError) as e:
         assert auth.auth_register('anotherinvalidemail', 'password', 'Register', 'Fails')
+
+    with pytest.raises(InputError) as e:
+        assert auth.auth_register('test@', 'password', 'Register', 'Fails')
+
+    with pytest.raises(InputError) as e:
+        assert auth.auth_register('@.com', 'password', 'Register', 'Fails')
+
+    with pytest.raises(InputError) as e:
+        assert auth.auth_register('!!##$$^^&@.com', 'password', 'Register', 'Fails')
+
+    with pytest.raises(InputError) as e:
+        assert auth.auth_register('', 'password', 'Register', 'Fails')
         
+def test_register_used_email_fail():
+    clear()
     with pytest.raises(InputError) as e:
         auth.auth_register('alreadyused@email.com', 'password', 'New', 'Person')
         assert auth.auth_register('alreadyused@email.com', 'password', 'Person', 'Another')
 
+def test_register_password_length_fail():
+    clear()
     with pytest.raises(InputError) as e:
         assert auth.auth_register('shortpassword@email.com', '12345', 'Short', 'Password')
 
+def test_register_name_limit_fail():
+    clear()
     with pytest.raises(InputError) as e:
         assert auth.auth_register('namelong@email.com', 'password', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz', 'surname')
 
     with pytest.raises(InputError) as e:
-        assert  auth.auth_register('surnamelong@email.com', 'password', 'name', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz')
+        assert auth.auth_register('namelong@email.com', 'password', '', 'surname')
 
+def test_register_surname_limit_fail():
+    clear()
     with pytest.raises(InputError) as e:
-        assert auth.auth_register('', 'password', 'Register', 'Fails')
+        assert  auth.auth_register('surnamelong@email.com', 'password', 'name', 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz')
+        
+    with pytest.raises(InputError) as e:
+        assert  auth.auth_register('surnamelong@email.com', 'password', 'name', '')
+
 
 def test_login_success():
     clear()
