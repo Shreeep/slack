8dcc1636c87abfbcf55398277a883f57b85c1b89
Wilson Guo <z5257866@student.unsw.edu.au>
2020-10-19 22:34:23 +1100

    Added rest of http_auth tests.

--- src/auth_http_test.py
+++ src/auth_http_test.py
@@ -5,12 +5,12 @@ from echo_http_test import url
 
 
 def test_post_register(url):
-	dataIn = {
-		'email': 'test@email.com',
-		'password': 'password123',
-		'name_first': 'Wilson',
-		'name_last': 'Guo'
-	}
+    dataIn = {
+        'email': 'test@email.com',
+        'password': 'password123',
+        'name_first': 'test',
+        'name_last': 'user'
+    }
 
     r = requests.post(url + "/auth/register", json=dataIn)
     user_token = r.json()
@@ -24,13 +24,52 @@ def test_post_register(url):
 
 
 def test_post_login(url):
-	r = requests.post(url + "/login", json=dataIn)
-	# result = r.json()
-    pass
+    dataIn = {
+        'email': 'test@email.com',
+        'password': 'password123',
 
+    }
+
+    r = requests.post(url + "/auth/login", json=dataIn)
+    user_token = r.json()
+    r = requests.get(url + "/user/profile", json=user_token)
+    result = r.json()
+    
+    assert result['email'] == dataIn['email']
+    assert result['name_first'] == dataIn['name_first']
+    assert result['name_last'] == dataIn['name_last']
 
 
 def test_post_logout(url):
-	r = requests.post(url + "/logout", json=dataIn)
-	# result = r.json()
-    pass
+
+    '''
+    Register user
+    Logout user
+    login with (expired) token
+    fail
+    '''
+    dataIn = {
+        'email': 'test@email.com',
+        'password': 'password123',
+        'name_first': 'test',
+        'name_last': 'user'
+    }
+
+    # register and login user
+    r = requests.post(url + "/auth/register", json=dataIn)
+
+    # return my u_id and token
+    user_token = r.json()
+
+    # log out with token
+    r = requests.post(url + "/auth/logout", json=user_token)
+
+    # successfully logged out
+    result = r.json()
+
+    assert result['is_success'] == True
+
+    # trying to log back in with previous token
+    # r = requests.get(url + "/user/profile", json=user_token)
+    # invalid = r.json()
+
