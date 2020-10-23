4f08a54f30d2bdd4d0535dcc1bb402092886d597
Wilson Guo <z5257866@student.unsw.edu.au>
2020-10-23 22:05:06 +1100

    Adding error testing and invalid inputs for functions.

--- src/user.py
+++ src/user.py
@@ -6,6 +6,7 @@ EMAIL_REGEX = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 MAX_NAME_LEN = 50
 MIN_NAME_LEN = 1
 MAX_HANDLE_LEN = 20
+MIN_HANDLE_LEN = 3
 
 def user_profile(token, u_id):
     
@@ -82,38 +83,47 @@ def user_profile_sethandle(token, handle_str):
     if token not in data.data['tokens']:
         raise AccessError
 
-    # # Generating user handle and limiting to 20 chars
-    # handle = name_first.lower() + name_last.lower()
-    # handle = handle[:MAX_HANDLE_LEN]
-    
-    # # if the handle has been used
-    # if handle in data.data['handles']:
-    #     # limiting handle to handle length - user_id_length, then adding user_id to the end
-    #     handle = handle[:MAX_HANDLE_LEN - len(str(data.user_id))] + str(data.user_id)
+    if len(handle_str) < MIN_HANDLE_LEN:
+        raise InputError
 
+    # # Generating user handle and limiting to 20 chars
+    handle_str = handle_str[:MAX_HANDLE_LEN]
+    
+    # check if handle is used
+    if handle_str in data.data['handles']:
+        # limiting handle to handle length - user_id_length, then adding user_id to the end
+        handle_str = handle_str[:MAX_HANDLE_LEN - len(str(data.user_id))] + str(data.user_id)
+    
+    # Saves the handle name
 
     user_id = data.data['tokens'][token]
     
     profile = data.data['users'][user_id]
 
     profile['handle_str'] = handle_str
+    data.data['handles'][handle_str] = True
 
-    # return profile
+    return profile
     return {
-    }
+    }   
 
-import auth
-# import json
-auth.auth_register('test@email.com', 'password', 'test', 'user')
-auth.auth_register('test1@email.com', 'password', 'test', 'user')
-print(auth.auth_register('test2@email.com', 'password', 'test', 'user'))
-# auth.auth_register('test3@email.com', 'password', 'test', 'user')
-# auth.auth_register('test4@email.com', 'password', 'test', 'user')
-print(user_profile('token3', 3))
+# import auth
+# # # import json
+# auth.auth_register('test@email.com', 'password', 'test', 'user')
+# auth.auth_register('test1@email.com', 'password', 'Another', 'user')
+# print(auth.auth_register('test2@email.com', 'password', 'New', 'user'))
+# # # auth.auth_register('test3@email.com', 'password', 'test', 'user')
+# # # auth.auth_register('test4@email.com', 'password', 'test', 'user')
+# # # print(user_profile('token3', 3))
 
-# print(user_profile_setname('token3', 'New', 'User'))
-# print(user_profile_setemail('token3', 'newemail@email.com'))
-# print(user_profile_sethandle('token3', 'thisisanewhandle'))
+# # print(user_profile_setname('token3', 'a', 'User'))
+# # # print(user_profile_setemail('token3', 'newemail@email.com'))
+# print(user_profile_sethandle('token3', 'testuser'))
+# print(user_profile_sethandle('token2', 'abcdefghijklmnopqrstuvwyzabcdefghijklmnopqrstuvwyz'))
+# print(user_profile_sethandle('token3', 'Anotheruser'))
+# print(user_profile_sethandle('token2', 'abcdefghijklmnopqrstuvwyzabcdefghijklmnopqrstuvwyz'))
+# print(user_profile_sethandle('token2', 'as'))
+# print(data.data['handles'])
 
 
 
--- src/user_test.py
+++ src/user_test.py
@@ -1,6 +1,6 @@
+import auth
 import user
 import pytest
-import auth
 from other import clear
 from error import InputError, AccessError
 
@@ -18,6 +18,20 @@ def test_user_profile():
         'handle_str': 'testuser2',
     }
 
+def test_user_profile_errors():
+    clear()
+    register1 = auth.auth_register('test@email.com', 'password', 'test', 'user')
+    register2 = auth.auth_register('test1@email.com', 'password', 'test', 'user')
+    register3 = auth.auth_register('test2@email.com', 'password', 'test', 'user')
+
+    # invalid u_id
+    with pytest.raises(InputError):
+        user.user_profile('token2', 10)
+
+    # invalid token
+    with pytest.raises(AccessError):
+        user.user_profile('token8', 2)
+
 def test_user_profile_setname():
     clear()
     register1 = auth.auth_register('test@email.com', 'password', 'test', 'user')
@@ -25,6 +39,29 @@ def test_user_profile_setname():
     register3 = auth.auth_register('test2@email.com', 'password', 'test', 'user')
     result = user.user_profile_setname('token2', 'New', 'UUSSSEEERRR')
     assert result == {}
+
+def test_user_profile_setname_errors():
+    clear()
+    register1 = auth.auth_register('test@email.com', 'password', 'test', 'user')
+    register2 = auth.auth_register('test1@email.com', 'password', 'test', 'user')
+    register3 = auth.auth_register('test2@email.com', 'password', 'test', 'user')
+
+    # invalid first name length
+    with pytest.raises(InputError):
+        user.user_profile_setname('token2', '', 'lastname')
+
+    # invalid first name length
+    with pytest.raises(InputError):
+        user.user_profile_setname('token2', 'abcdefghijklmnopqrstuvwyzabcdefghijklmnopqrstuvwyz', 'lastname')
+
+    # invalid last name length
+    with pytest.raises(InputError):
+        user.user_profile_setname('token2', 'firstname', '')
+
+    # invalid first name length
+    with pytest.raises(InputError):
+        user.user_profile_setname('token2', 'firstname', 'abcdefghijklmnopqrstuvwyzabcdefghijklmnopqrstuvwyz')
+
 
 def test_user_profile_setemail():
     clear()
@@ -34,6 +71,44 @@ def test_user_profile_setemail():
     result = user.user_profile_setemail('token2', 'anewemail@email.com')
     assert result == {}
 
+def test_user_profile_setemail_errors():
+    clear()
+    register1 = auth.auth_register('test@email.com', 'password', 'test', 'user')
+    register2 = auth.auth_register('test1@email.com', 'password', 'test', 'user')
+    register3 = auth.auth_register('test2@email.com', 'password', 'test', 'user')
+
+    # no @ symbol
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token2', 'anewemailemail.com')
+
+    # no .com
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token2', 'anewemail@email')
+
+    # no string before @
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token2', '@email.com')
+
+    # using symbols
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token2', '!@#$%^@email.com')
+
+    # no . after @ 
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token2', 'anewemail@emailcom')
+
+    # empty input
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token2', '')
+
+def test_user_profile_setemail_used():
+    clear()
+    auth.auth_register('test@email.com', 'password', 'New', 'Person')
+
+    # used email
+    with pytest.raises(InputError):
+        user.user_profile_setemail('token1', 'test@email.com')
+        
 def test_user_profile_sethandle():
     clear()
     register1 = auth.auth_register('test@email.com', 'password', 'test', 'user')
@@ -41,3 +116,12 @@ def test_user_profile_sethandle():
     register3 = auth.auth_register('test2@email.com', 'password', 'test', 'user')
     result = user.user_profile_sethandle('token2', 'thisisanewhandle')
     assert result == {}
+
+def test_user_profile_sethandle_errors():
+    clear()
+    register1 = auth.auth_register('test@email.com', 'password', 'test', 'user')
+    register2 = auth.auth_register('test1@email.com', 'password', 'test', 'user')
+    register3 = auth.auth_register('test2@email.com', 'password', 'test', 'user')
+
+    with pytest.raises(InputError):
+        user.user_profile_sethandle('token2', 'ab')
