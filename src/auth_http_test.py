09701f57540c919fcb7f2dfafb6b809a33ad3041
Wilson Guo <z5257866@student.unsw.edu.au>
2020-10-24 15:00:22 +1100

    Finished adding rest of auth_http tests.

--- src/auth_http_test.py
+++ src/auth_http_test.py
@@ -29,8 +29,6 @@ def url():
         raise Exception("Couldn't get URL from local server")
 
 
-
-
 def test_post_register(url):
  
     # user info
@@ -116,92 +114,10 @@ def test_post_login(url):
     assert user['name_first'] == dataIn['name_first']
     assert user['name_last'] == dataIn['name_last']
 
+
 def test_post_register_multiple(url):
     
-    # user info
-    dataIn = {
-        'email': 'test@email.com',
-        'password': 'password123',
-        'name_first': 'test',
-        'name_last': 'user'
-    }
-
-    # register user
-    r = requests.post(url + "/auth/register", json=dataIn)
-    encoded_jwt = r.json()
-
-    dataIn2 = {
-        'email': 'test2@email.com',
-        'password': 'password123456',
-        'name_first': 'New',
-        'name_last': 'User'
-    }
-
-    # register user
-    r2 = requests.post(url + "/auth/register", json=dataIn2)
-    encoded_jwt2 = r.json()
-
-    # checking for valid token (user 1)
-    r = requests.get(url + "/users/all", params={'token': encoded_jwt['token']})
-    result = r.json()
-
-    user = result['users'][0]
-
-    assert user['email'] == dataIn['email']
-    assert user['name_first'] == dataIn['name_first']
-    assert user['name_last'] == dataIn['name_last']
-
-    # checking for valid token (user 2)
-    r2 = requests.get(url + "/users/all", params={'token': encoded_jwt2['token']})
-    result = r.json()
-
-    user = result['users'][1]
-
-    assert user['email'] == dataIn2['email']
-    assert user['name_first'] == dataIn2['name_first']
-    assert user['name_last'] == dataIn2['name_last']
-
-def test_post_logout_multiple(url):
-
-    # user info
-    dataIn = {
-        'email': 'test@email.com',
-        'password': 'password123',
-        'name_first': 'test',
-        'name_last': 'user'
-    }
-
-    # register and login user
-    r = requests.post(url + "/auth/register", json=dataIn)
-    encoded_jwt = r.json()
-
-
-    dataIn2 = {
-        'email': 'test2@email.com',
-        'password': 'password123456',
-        'name_first': 'New',
-        'name_last': 'User'
-    }
-
-    # register user 2
-    r2 = requests.post(url + "/auth/register", json=dataIn2)
-    encoded_jwt2 = r.json()
-
-    # log out with token (user 1)
-    r = requests.post(url + "/auth/logout", json=encoded_jwt)
-    result = r.json()
-
-    # successfully logged out
-    assert result['is_success'] == True
-
-    r2 = requests.post(url + "/auth/logout", json=encoded_jwt2)
-    result2 = r.json()
-
-    assert result2['is_success'] == True
-
-def test_post_login_multiple(url):
-   
-    # user info
+    # user info 1
     dataIn = {
         'email': 'test@email.com',
         'password': 'password123',
@@ -222,43 +138,186 @@ def test_post_login_multiple(url):
 
     # register user 2
     r2 = requests.post(url + "/auth/register", json=dataIn2)
-    encoded_jwt2 = r.json()
+    encoded_jwt2 = r2.json()
 
-    # log out with token
+    # checking for valid token (user 2)
+    r2 = requests.get(url + "/users/all", params={'token': encoded_jwt2['token']})
+    result = r2.json()
+
+    # user 1 info
+    user = result['users'][0]
+    assert user['email'] == dataIn['email']
+    assert user['name_first'] == dataIn['name_first']
+    assert user['name_last'] == dataIn['name_last']
+
+    # user 2 info
+    user2 = result['users'][1]
+    assert user2['email'] == dataIn2['email']
+    assert user2['name_first'] == dataIn2['name_first']
+    assert user2['name_last'] == dataIn2['name_last']
+
+
+def test_post_logout_multiple(url):
+
+    # user info 1
+    dataIn = {
+        'email': 'test@email.com',
+        'password': 'password123',
+        'name_first': 'test',
+        'name_last': 'user'
+    }
+
+    # register and login user 1
+    r = requests.post(url + "/auth/register", json=dataIn)
+    encoded_jwt = r.json()
+
+    # user info 2
+    dataIn2 = {
+        'email': 'test2@email.com',
+        'password': 'password123456',
+        'name_first': 'New',
+        'name_last': 'User'
+    }
+
+    # register user 2
+    r2 = requests.post(url + "/auth/register", json=dataIn2)
+    encoded_jwt2 = r2.json()
+
+    # log out with token (user 1)
+    r = requests.post(url + "/auth/logout", json=encoded_jwt)
+    result = r.json()
+
+    # successfully logged out
+    assert result['is_success'] == True
+
+    # checking for valid token (user 2)
+    r2 = requests.get(url + "/users/all", params={'token': encoded_jwt2['token']})
+    result = r2.json()
+
+    # user 2 info
+    user2 = result['users'][1]
+    assert user2['email'] == dataIn2['email']
+    assert user2['name_first'] == dataIn2['name_first']
+    assert user2['name_last'] == dataIn2['name_last']  
+
+    r2 = requests.post(url + "/auth/logout", json=encoded_jwt2)
+    result2 = r2.json()
+
+    assert result2['is_success'] == True
+
+
+def test_post_login_multiple(url):
+   
+    # user info 1
+    dataIn = {
+        'email': 'test@email.com',
+        'password': 'password123',
+        'name_first': 'test',
+        'name_last': 'user'
+    }
+
+    # register user 1
+    r = requests.post(url + "/auth/register", json=dataIn)
+    encoded_jwt = r.json()
+
+    # user info 2
+    dataIn2 = {
+        'email': 'test2@email.com',
+        'password': 'password123456',
+        'name_first': 'New',
+        'name_last': 'User'
+    }
+
+    # register user 2
+    r2 = requests.post(url + "/auth/register", json=dataIn2)
+    encoded_jwt2 = r2.json()
+
+    # log out both users
     r = requests.post(url + "/auth/logout", json=encoded_jwt)
     r2 = requests.post(url + "/auth/logout", json=encoded_jwt2)
 
-    # user login info
+    # user login info 1
     login_data = {
         'email': 'test@email.com',
         'password': 'password123',
 
     }
 
-    # logging in
-    r = requests.post(url + "/auth/login", json=login_data)
-    encoded_jwt = r.json()
-
-    # checking correct user
-    r = requests.get(url + "/users/all", params={'token': encoded_jwt['token']})
-    result = r.json()
-
-    user = result['users'][0]
-    
-    assert user['email'] == dataIn['email']
-    assert user['name_first'] == dataIn['name_first']
-    assert user['name_last'] == dataIn['name_last']
-
+    # user login info 2
     login_data2 = {
         'email': 'test2@email.com',
         'password': 'password123456',
 
     }
 
-    # logging in
-    r2 = requests.post(url + "/auth/login", json=login_data2)
+    # login user 1
+    r = requests.post(url + "/auth/login", json=login_data)
     encoded_jwt = r.json()
+
+    # login user 2
+    r2 = requests.post(url + "/auth/login", json=login_data2)
+    encoded_jwt2= r2.json()
 
     # checking correct user
     r2 = requests.get(url + "/users/all", params={'token': encoded_jwt2['token']})
+    result2 = r2.json()
+
+    # user 1 info
+    user = result2['users'][0]
+    assert user['email'] == dataIn['email']
+    assert user['name_first'] == dataIn['name_first']
+    assert user['name_last'] == dataIn['name_last']
+
+    # user 2 info
+    user2 = result2['users'][1]
+    assert user2['email'] == dataIn2['email']
+    assert user2['name_first'] == dataIn2['name_first']
+    assert user2['name_last'] == dataIn2['name_last']
+
+
+def test_post_register_errors(url):
+
+    # user info 1
+    dataIn = {
+        'email': 'testemail.com',
+        'password': 'password123',
+        'name_first': 'test',
+        'name_last': 'user'
+    }
+
+    # register user 1
+    r = requests.post(url + "/auth/register", json=dataIn)
+
+    assert r.status_code == 400
+
+
+def test_post_login_errors(url):
+
+    # user info 1
+    dataIn = {
+        'email': 'test@email.com',
+        'password': 'password123',
+        'name_first': 'test',
+        'name_last': 'user'
+    }
+
+    # register and login user
+    r = requests.post(url + "/auth/register", json=dataIn)
+    encoded_jwt = r.json()
+
+    # log out with token
+    r = requests.post(url + "/auth/logout", json=encoded_jwt)
     result = r.json()
+
+    # user login info
+    login_data = {
+        'email': 'test@email.com',
+        'password': 'password',
+
+    }
+
+    # logging in
+    r = requests.post(url + "/auth/login", json=login_data)
+
+    assert r.status_code == 400
+
