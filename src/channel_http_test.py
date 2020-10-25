65665fdc7a5937f0c83164b15d7658c679a59a1b
z5209699 <z5209699@cse.unsw.edu.au>
2020-10-25 20:11:06 +1100

    tests added for channel/leave and channel/join

---
+++ src/channel_http_test.py
@@ -0,0 +1,130 @@
+import requests
+import json
+import pytest
+import re
+import signal
+import jwt
+from subprocess import Popen, PIPE
+from time import sleep
+
+
+@pytest.fixture
+def url():
+    url_re = re.compile(r' \* Running on ([^ ]*)')
+    server = Popen(["python3", "src/server.py"], stderr=PIPE, stdout=PIPE)
+    line = server.stderr.readline()
+    local_url = url_re.match(line.decode())
+    if local_url:
+        yield local_url.group(1)
+        # Terminate the server
+        server.send_signal(signal.SIGINT)
+        waited = 0
+        while server.poll() is None and waited < 5:
+            sleep(0.1)
+            waited += 0.1
+        if server.poll() is None:
+            server.kill()
+    else:
+        server.kill()
+        raise Exception("Couldn't get URL from local server")
+
+def test_post_channel_leave(url):
+    #tests if channel_leave works
+
+    # user info
+    user1 = {
+        'email': 'crazyfrog@email.com',
+        'password': 'dingding321321',
+        'name_first': 'crazy',
+        'name_last': 'frog'
+    }
+
+    # register user
+    register_user1 = requests.post(url + "/auth/register", json=user1)
+    encoded_jwt_user1 = register_user1.json()
+
+    channel1 = {
+        'token' : encoded_jwt_user1['token'],
+        'name' : 'CrazyFrogsNewChannel',
+        'is_public' : True
+    }
+
+    #create channel
+    create_channel_one = requests.post(url + "/channels/create", json=channel1)
+    encoded_channel_one = create_channel_one.json()
+
+    #leave channel inputs
+    u1_leave_ch1_data = {
+        'token' : encoded_jwt_user1['token'],
+        'channel_id' : encoded_channel_one['channel_id']
+    }
+    #user 1 leaves channel 1
+    user1_leave = requests.post(url + "channel/leave", json=u1_leave_ch1_data)
+    
+    #assert that user1 is no longer part of channel1 (or any channels)
+    user_list = requests.get(url + "channels/list", data={'token' : encoded_jwt_user1['token']})
+    user1_channels = user_list.json()
+
+    assert user1_channels['channels'] == []
+
+
+def test_post_channel_join(url):
+    #tests if channel_leave works
+
+    # user info
+    user1 = {
+        'email': 'crazyfrog@email.com',
+        'password': 'dingding321321',
+        'name_first': 'crazy',
+        'name_last': 'frog'
+    }
+
+    # register user1
+    register_user1 = requests.post(url + "/auth/register", json=user1)
+    encoded_jwt_user1 = register_user1.json()
+
+    # user info
+    user2 = {
+        'email': 'bigchungus@email.com',
+        'password': 'raspberrypi321321',
+        'name_first': 'bugs',
+        'name_last': 'bunny'
+    }
+
+    # register user2
+    register_user2 = requests.post(url + "/auth/register", json=user2)
+    encoded_jwt_user2 = register_user2.json()
+
+
+    channel1 = {
+        'token' : encoded_jwt_user1['token'],
+        'name' : 'CrazyFrogsNewChannel',
+        'is_public' : True
+    }
+
+    #user1 creates channel
+    create_channel_1 = requests.post(url + "/channels/create", json=channel1)
+    encoded_channel_1 = create_channel_1.json()
+
+    #channel join inputs
+    u2_join_ch1_data = {
+        'token' : encoded_jwt_user2['token'],
+        'channel_id' : encoded_channel_1['channel_id']
+    }
+    #user 1 leaves channel 1
+    user2_join = requests.post(url + "channel/join", json=u2_join_ch1_data)
+    
+    #assert that user2 is part of channel1
+    user_list_2 = requests.get(url + "channels/list", data={'token' : encoded_jwt_user1['token']})
+    user2_channels = user_list_2.json()
+
+    assert user2_channels['channels'] == [
+        {
+            'channel_id': encoded_channel_1['channel_id'],
+            'name' : 'CrazyFrogsNewChannel'
+        }
+    ]
+
+
+
+
