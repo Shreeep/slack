cf9bbf3729e1df7817433d03706ca295cfe1ccce
z5206651 <h.hasam@student.unsw.edu.au>
2020-10-25 21:33:37 +1100

    Finished Wrapping implementation - ready to test and debug to get Pipeline passing

--- src/server.py
+++ src/server.py
@@ -5,6 +5,7 @@ from flask_cors import CORS
 from error import InputError
 import auth
 import channels
+import message
 import other
 import hashlib
 import jwt
@@ -131,6 +132,31 @@ def create():
         'channel_id': channel['channel_id']
     }
     return dumps(result)
+
+@APP.route("/message/send", methods=['POST'])
+def send_message():
+    info = request.get_json()
+    decoded_jwt = jwt.decode(info['token'], data.jwt_secret, algorithm='HS256')
+    message = message.message_send(decoded_jwt['token'], info['channel_id'],  info['message'])
+    result = {
+        'message_id': message['message_id']
+    }
+    return dumps(result)
+
+@APP.route("/message/remove", methods=['DELETE'])
+def remove_message():
+    info = request.get_json()
+    decoded_jwt = jwt.decode(info['token'], data.jwt_secret, algorithm='HS256')
+    message.message_remove(decoded_jwt['token'], info['message_id'])
+    return {}
+
+@APP.route("/message/edit", methods=['PUT'])
+def edit_message():
+    info = request.get_json()
+    decoded_jwt = jwt.decode(info['token'], data.jwt_secret, algorithm='HS256')
+    message.message_edit(decoded_jwt['token'], info['message_id'], info['message'])
+    return {}
+
 
 if __name__ == "__main__":
     #APP.run(port=0) # Do not edit this port
