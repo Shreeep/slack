9ddbe24b8da4f64637952edab15bcba0cb087edd
z5209699 <z5209699@cse.unsw.edu.au>
2020-10-25 22:51:09 +1100

    shree's channel function wrappers moved to server.py to prepare for http testing

--- src/server.py
+++ src/server.py
@@ -104,6 +104,49 @@ def users_all():
 
     return {'users': all_users}
     
+@APP.route("/channel/leave", methods=['POST'])
+def leave():
+    # get user info
+    token = request.args['token']  
+    decoded_jwt = jwt.decode(token, data.jwt_secret, algorithm='HS256')
+    channel = request.args['channel_id']
+
+    result = channel.channel_leave(decoded_jwt['token'], channel)
+
+    return dumps(result)
+
+@APP.route("/channel/join", methods=['POST'])
+def join():
+    # get user info
+    token = request.args['token']  
+    decoded_jwt = jwt.decode(token, data.jwt_secret, algorithm='HS256')
+    channel = request.args['channel_id']
+
+    result = channel.channel_join(decoded_jwt['token'], channel)
+    return dumps(result)
+
+@APP.route("/channel/addowner", methods=['POST'])
+def addowner():
+    # get user info
+    token = request.args['token']  
+    decoded_jwt = jwt.decode(token, data.jwt_secret, algorithm='HS256')
+    channel = request.args['channel_id']
+    user_id = request.args['u_id']
+
+    result = channel.channel_addowner(decoded_jwt['token'], channel, user_id)
+    return dumps(result)
+
+@APP.route("/channel/removeowner", methods=['POST'])
+def removeowner():
+    # get user info
+    token = request.args['token']  
+    decoded_jwt = jwt.decode(token, data.jwt_secret, algorithm='HS256')
+    channel = request.args['channel_id']
+    user_id = request.args['u_id']
+
+    result = channel.channel_removeowner(decoded_jwt['token'], channel, user_id)
+
+    return dumps(result)
 
 
 if __name__ == "__main__":
