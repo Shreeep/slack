24e74d1365f3839c62e03b09acc1af09840c8ca7
z5206651 <h.hasam@student.unsw.edu.au>
2020-11-10 18:40:40 +1100

    Finished implementation for message react, onto testing if it works

--- src/message.py
+++ src/message.py
@@ -1,6 +1,7 @@
 from error import InputError, AccessError
 import data
 import datetime
+import channels
 
 def message_send(token, channel_id, message):
     #Check if message length is not more than 1000.
@@ -22,7 +23,12 @@ def message_send(token, channel_id, message):
         'u_id': user_id,
         'message': message,
         'time_created': time_created,
-        'reacts':[], 
+        'reacts':[{ 
+                    'react_id': 0,
+                    'u_ids':[],
+                    'is_this_user_reacted': False,
+                },
+        ], 
     }
     
     #Append the new_message to data
@@ -70,8 +76,28 @@ def message_edit(token, message_id, message):
     return {}
 
 def message_react(token, message_id, react_id):
-def message_sendlater(token, channel_id, message, time_sent):
-def message_unreact(token, message_id, react_id):
+    if (react_id != 1):
+        raise InputError 
+    user_id = data.data['tokens'][token]
+    #Check if given message_id is valid - if not, raise InputError
+    channel_id = check_which_channel_message_is_in(message_id) 
+    #Check if user is apart of the channel that the message is in - if not, raise Access Error 
+    check_if_valid_channel_and_member(channel_id, user_id)
+    #Check if user has already reacted to that particular message - if not, raise InputError 
+    check_if_user_already_reacted(channel_id, message_id, user_id)
+    #Set react_id, add user_id to u_ids list, and if the user is the original sender, set _is_this_user_reacted to True  
+    for channel in data.data['channels']:
+        if channel_id == channel['id']:
+            for message in channel['messages']: 
+                if message_id == message['message_id']: 
+                    message['reacts'][0]['react_id'] = react_id
+                    message['reacts'][0]['u_ids'].append(user_id)
+                    if message['u_id'] == user_id:
+                        message['reacts'][0]['is_this_user_reacted'] = True 
+    return {}
+
+#def message_sendlater(token, channel_id, message, time_sent):
+#def message_unreact(token, message_id, react_id):
 
 #Credit: Taken from channel.py - the file Shree and Vignaraj have worked on
 #The function checks if the user is a member of the channel. 
@@ -97,3 +123,18 @@ def check_if_user_is_owner(channel_id, user_id):
             if not any(owner['u_id'] == user_id for owner in channel['owners']):
                 return False
     return True
+
+def check_which_channel_message_is_in(message_id):
+    for channel in data.data['channels']:
+        for message in channel['messages']:
+            if message['message_id'] == message_id: 
+                return channel['id']
+        raise InputError #raises input error if message_id does not exist 
+
+def check_if_user_already_reacted(channel_id, message_id, user_id): 
+    for channel in data.data['channels']: 
+        if channel_id == channel['id']:
+            for messages in channel['messages']:
+                if message_id == messages['message_id']: 
+                    if user_id in message['reacts'][0]['u_ids']:
+                        raise InputError
